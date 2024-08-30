c6f6a1e714aeca5cc3a2ed4e31dd24b0e2b1ca08
Subtain Khan <subtainkhan5656@gmail.com>
2024-08-30 18:57:02 +0500

    Update apis.dart

--- lib/APIs/apis.dart
+++ lib/APIs/apis.dart
@@ -1,13 +1,1 @@
 
-
-
-String login = '$Baseurl/login.php';
-
-
-
-String InstallarStatus = '$Baseurl/InstallarStatus.php';
-String Assigned = '$Baseurl/Assigned.php';
-String Pending = '$Baseurl/Pending.php';
-String Installed = '$Baseurl/Installed.php';
-String GetComplain = '$Baseurl/GetComplain.php';
-String SaveVisit = '$Baseurl/SaveVisit.php';
